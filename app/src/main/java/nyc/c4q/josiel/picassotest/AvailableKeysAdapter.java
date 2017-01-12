7af5fd62b0e4146716137e02a85b11aebb940218
Josiel <jdlopez114@gmail.com>
2017-01-11 19:54:07 -0500

    adapter, view holder

--- app/src/main/java/nyc/c4q/josiel/picassotest/AvailableKeysAdapter.java
+++ app/src/main/java/nyc/c4q/josiel/picassotest/AvailableKeysAdapter.java
@@ -1,23 +1,38 @@
 package nyc.c4q.josiel.picassotest;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.List;
+
+import nyc.c4q.josiel.picassotest.Backend.AvailableKeys;
 
 public class AvailableKeysAdapter extends RecyclerView.Adapter<AvailableKeysViewHolder> {
 
+    List<AvailableKeys> availableKeysList;
+
+    public AvailableKeysAdapter(List<AvailableKeys> availableKeysList){
+        this.availableKeysList = availableKeysList;
+    }
 
     @Override
     public AvailableKeysViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_view, parent, false);
+
+        return new AvailableKeysViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(AvailableKeysViewHolder holder, int position) {
-
+        AvailableKeys myAvailableKeys = availableKeysList.get(position);
+        holder.bind(myAvailableKeys);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return availableKeysList.size();
     }
 }
--- app/src/main/java/nyc/c4q/josiel/picassotest/AvailableKeysViewHolder.java
+++ app/src/main/java/nyc/c4q/josiel/picassotest/AvailableKeysViewHolder.java
@@ -1,11 +1,31 @@
 package nyc.c4q.josiel.picassotest;
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
+
+import nyc.c4q.josiel.picassotest.Backend.AvailableKeys;
 
 
 public class AvailableKeysViewHolder extends RecyclerView.ViewHolder {
+
+    TextView keyName;
+
     public AvailableKeysViewHolder(View itemView) {
         super(itemView);
+        keyName = (TextView) itemView.findViewById(R.id.key_name);
+    }
+
+    public void bind(AvailableKeys myAvailableKeys) {
+        keyName.setText(myAvailableKeys.getName());
+        keyName.setTextColor(Color.parseColor(myAvailableKeys.getTextColor()));
+        itemView.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
